6dc0aa1bbc01f1b08307bbc2e8c5e06e06a39744
gormitansh@gmail.com <gormitansh@gmail.com>
2022-02-27 02:35:43 +0530

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(express.static(__dirname + 'client/build'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'client/build/index.html'));
+  app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
+
 });
 
 // Start the app by listening on the default Heroku port
