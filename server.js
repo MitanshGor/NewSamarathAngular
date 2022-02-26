cabafefdf186ab70d80e62ad63102697d80c01f7
gormitansh@gmail.com <gormitansh@gmail.com>
2022-02-27 03:06:44 +0530

    make it better

--- server.js
+++ server.js
@@ -6,22 +6,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'client/build'));
+app.use(express.static(__dirname + '/client/build'));
 
+app.get('/*', function(req,res) {
 
-app.get('/*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
+res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
-app.get('/app/:id', checkUserAuth, findApp, renderView, sendJSON);
-
-function checkUserAuth(req, res, next) {
-  if (req.session.user) return next();
-  return next(new NotAuthorizedError());
-}
-
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
