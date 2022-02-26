5f3cb54a09b4d34003c74e8a64a16c64b9c181c5
gormitansh@gmail.com <gormitansh@gmail.com>
2022-02-27 00:58:05 +0530

    make it better

--- src/app/main-page-module/main-page-module-routing.module.ts
+++ src/app/main-page-module/main-page-module-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { OfficeBearerImageComponentComponent } from './office-bearer-image-component/office-bearer-image-component.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,
+  ]
 })
 export class AppRoutingModule { }
--- src/app/main-page-module/main-page-module.module.ts
+++ src/app/main-page-module/main-page-module.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OfficeBearerImageComponentComponent } from './office-bearer-image-component/office-bearer-image-component.component';
 
 
 @NgModule({
   declarations: [
-  
-    OfficeBearerImageComponentComponent
+
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
   ]
 })
 export class MainPageModuleModule { }
