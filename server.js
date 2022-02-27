f546ad1af91ce8719f6db62355a8ae0aa72f13b4
gormitansh@gmail.com <gormitansh@gmail.com>
2022-02-27 14:15:06 +0530

    make it better

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/newSamarathSchoolFrontend/'));
+app.use(express.static(__dirname + '/dist/new-samarath-school-frontend/'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/newSamarathSchoolFrontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/new-samarath-school-frontend/index.html'));
 });
 
 
