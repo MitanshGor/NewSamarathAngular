a851a1c7e8de5a5271995c5dbac2c3c10cf1f256
gormitansh@gmail.com <gormitansh@gmail.com>
2022-02-27 13:02:24 +0530

    make it better

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/src/index.html'));
 });
+console.log(__dirname+'/src/index.html');
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
